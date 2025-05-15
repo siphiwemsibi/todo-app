455e97ae26931f474b83fe228b17f81b37ad1b6d
Siphiwe Msibi <siphiwe.msibi@live.com>
2025-05-15 13:29:46 +0200

    Implement "Add Todo features" on the web app

--- web.py
+++ web.py
@@ -1,15 +1,24 @@
 import streamlit as st
 import functions
 
+#Get the Todo List
 todos = functions.get_todos()
 
+#Function to update the Todo List, using session_state data
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+#
 st.title("Siphiwe's Todo App")
 st.subheader("This is my Todo App")
 
-
+#Format the todo list using checkbox
 for todo in todos:
     st.checkbox(todo)
 
 
 #input
-st.text_input(label="", placeholder="Add new todo..")
+st.text_input(label="", placeholder="Add new todo..", 
+              on_change=add_todo, key="new_todo")
